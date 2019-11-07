07264f8f218334d8036e6d1472e29edaa6230678
HirushaR <hirusharandunu11@gmail.com>
2019-11-07 17:07:26 +0530

    open function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import wx
 import wx.lib.dialogs
 import wx.stc as stc
+import os
 
 fasec = {
     'times': 'Times new Roman',
@@ -78,6 +79,23 @@ class MainWindow(wx.Frame):
     def OnNew(self, e):
         self.filename = ''
         self.control.SetValue("")
+
+    def OnOpen(self, e):
+        try:
+            dlg = wx.FileDialog(self, "Choose a file", self.dirname,"", "*.*",wx.FD_OPEN)
+                                     # title ,direcoty,type,id
+            if(dlg.ShowModal() == wx.ID_OK):
+                self.filename = dlg.GetFilename()
+                self.dirname = dlg.GetDirectory()
+                f = open(os.path.join(self.dirname, self.filename), 'r')
+                self.control.SetValue(f.read())
+                f.close()
+            dlg.Destroy()
+        except:
+            dlg = wx.MessageDialog(self, "Coudn't open the file", "Error", wx.ICON_ERROR)
+            dlg.ShowModal()
+            dlg.Destroy()
+
 
 
 
